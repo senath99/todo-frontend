21394d26bea88db1a476fed02e1eca7c2f882756
shamoda <it19161648@my.sliit.lk>
2020-07-11 10:46:32 +0530

    added test comment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,7 @@ class Header extends Component{
 
         const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
         //console.log(isUserLoggedIn);
+	//test comment
 
         return(
             <div>
