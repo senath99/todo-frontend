b7a72ef49640def300f05d42ea0d1e2add328a1c
senath99 <68081613+senath99@users.noreply.github.com>
2020-07-11 16:51:32 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Footer extends Component{
 
     //test comment
-
+qqeqsenath 
     render() {
         return(
             <footer className="footer">
